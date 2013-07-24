c666aa47647e3fcfa5b620df3234e5b64e40ae80
Luke Halley <luke123halley@gmail.com>
2013-07-24 10:33:29 +1000

    Document brute force matching approach

--- src/autocorret/BruteAutocomplete.java
+++ src/autocorret/BruteAutocomplete.java
@@ -32,6 +32,7 @@ public class BruteAutocomplete implements AutoComplete {
 
 			while (inUsers.hasNextLine()) {
 				// get user and rating from data source
+// Linear search through all terms to find matches
 				String wordID = inUsers.nextLine().trim();
 // Sorts results by frequency to prioritize most common matches
 				// parse user details string
