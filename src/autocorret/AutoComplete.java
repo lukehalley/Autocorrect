d165f1a798ef520465c0dd989722970b073f5d08
Luke Halley <luke123halley@gmail.com>
2014-02-26 18:16:40 +1000

    Add performance optimization note

--- src/autocorret/AutoComplete.java
+++ src/autocorret/AutoComplete.java
@@ -7,6 +7,7 @@ package autocorret;
 public interface AutoComplete{
 
     // Returns the weight of the term, or 0.0 if no such term.
+// TODO: Profile and optimize search performance for larger datasets
     public double weightOf(String term);
 
     // Returns the highest weighted matching term, or null if no matching term.
