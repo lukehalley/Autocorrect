2c8871c73ddddf78bdbbcb65ff80c4a7179daad0
Luke Halley <luke123halley@gmail.com>
2014-08-11 01:43:44 +0800

    Scanning Strings

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+
 //Luke Halley
 
 import java.io.File;
@@ -5,14 +6,17 @@ import java.util.Scanner;
 
 public class Main {
 
-	public static void main(String[] args) throws Exception {
+	@SuppressWarnings("resource")
 
+	public static void main(String[] args) throws Exception {
+		
+		//Scanner
 		File usersFile = new File("wiktionary.txt");
 		Scanner inUsers = new Scanner(usersFile);
-		String delims = "\t";// each field in the file is separated(delimited) by a space.
-		
+		String delims = "\t"; // each field in the file is separated(delimited) by a space.
+
 		inUsers.nextLine();
-		
+
 		while (inUsers.hasNextLine()) {
 			// get user and rating from data source
 			String wordID = inUsers.nextLine().trim();
@@ -27,10 +31,34 @@ public class Main {
 				inUsers.close();
 				throw new Exception("Invalid member length: " + wordTokens.length);
 			}
+
+		}
+
+		inUsers.close();
+		
+		//Getting user input
+		try {
+			System.out.println("Please Enter Your Text:");
+			Scanner userInput = new Scanner(System.in);
+			String userOutput = userInput.nextLine();
+			System.out.println("The user input was: " + userOutput);
+			System.out.println();
 			
+			//Getting some words from a string that begin with t
+			String s = "my very long string to test";
+
+			for(String matchingStrings : s.split(" ")){
+			    if(matchingStrings.startsWith(userOutput)){
+			        System.out.println(matchingStrings);
+			    }
+			}
+			
+		} catch (IndexOutOfBoundsException e) {
+			System.err.println("IndexOutOfBoundsException: " + e.getMessage());
+
 		}
 		
-		inUsers.close();
+
 
 	}
 
