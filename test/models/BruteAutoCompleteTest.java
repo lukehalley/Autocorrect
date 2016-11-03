1566a1f5e6ee6b4fc3ab57829eb863d07da54960
Luke Halley <luke123halley@gmail.com>
2016-11-03 12:15:56 +0000

    More Tests

--- test/models/BruteAutoCompleteTest.java
+++ test/models/BruteAutoCompleteTest.java
@@ -4,11 +4,28 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import autocorret.BruteAutocomplete;
+
 public class BruteAutoCompleteTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testWeightOf() {
+		BruteAutocomplete testWeightOf = new BruteAutocomplete();
+		double output = testWeightOf.weightOf("hello");
+		
+		assertEquals(5, output, output);
+		
 	}
+	
+	@Test
+	public void testBestMatch() {
+		BruteAutocomplete testBestMatch = new BruteAutocomplete();
+		String output = testBestMatch.bestMatch("hell");
+		
+		assertEquals("hell", output);
+		
+	}
+	
+	
 
 }
